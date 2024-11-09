e02b4306bab1e5b1f78a6364801f56ccc394721b
elBukhara <shakhzodgiazov@gmail.com>
2024-11-09 17:00:03 +0300

    added: username and password validation in login page

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -12,6 +12,7 @@ function LoginForm() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(""); // Error message state
 
   const navigate = useNavigate();
 
@@ -19,56 +20,60 @@ function LoginForm() {
     setLoading(true);
     event.preventDefault();
 
+    // Clear any previous error messages
+    setErrorMessage("");
+
     try {
       const response = await api.post("/auth/login/", {
         username,
         password,
       });
 
-      console.log(response.data);
-      
       if (response.status === 200) {
         localStorage.setItem(ACCESS_TOKEN, response.data.access);
         localStorage.setItem(REFRESH_TOKEN, response.data.refresh);
         login();
         navigate("/home");
-      } 
+      }
 
     } catch (error) {
-      alert(error);
+      if (error.response && error.response.status === 401) {
+        setErrorMessage("Invalid username or password.");
+      } else {
+        setErrorMessage("An error occurred. Please try again.");
+      }
     } finally {
       setLoading(false);
     }
-
   };
 
   return (
     <form onSubmit={handleSubmit} className="form-container">
-        <h1>Login</h1>
-        <input
-            className="form-input"
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="Username"
-        />
-        <input
-            className="form-input"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
-        />
-        {loading && <LoadingIndicator />}
-        <button className="form-button" type="submit">
-            Login
-        </button>
-        <p>
-            Don't have an account yet? <Link to="/register">Register</Link>
-        </p>
+      <h1>Login</h1>
+      {errorMessage && <div className="alert alert-danger">{errorMessage}</div>}
+      <input
+        className="form-input"
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Username"
+      />
+      <input
+        className="form-input"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      {loading && <LoadingIndicator />}
+      <button className="form-button" type="submit">
+        Login
+      </button>
+      <p>
+        Don't have an account yet? <Link to="/register">Register</Link>
+      </p>
     </form>
   );
 }
 
-
-export default LoginForm
+export default LoginForm;
